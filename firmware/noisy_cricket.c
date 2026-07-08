10e21447cb2cdc38dd80f3f1b1f093c80924f72d
Brent Mellor <brentmellor@gmail.com>
2026-07-08 16:47:34 -0600

    working wavegen code on dev board

--- firmware/noisy_cricket.c
+++ firmware/noisy_cricket.c
@@ -5,35 +5,11 @@
 
 uint32_t count;
 
-#define LED_R_PORT GPIOD
-#define LED_R_PIN 2
+#define AUDIO_PORT GPIOD
+#define AUDIO_PIN 2
 
-#define LED_G_PORT GPIOD
-#define LED_G_PIN 3
-
-#define LED_B_PORT GPIOD
-#define LED_B_PIN 4
-
-#define UART_TX_PORT GPIOC
-#define UART_TX_PIN 0
-
-#define SDA_PORT GPIOC
-#define SDA_PIN 1
-
-#define SCL_PORT GPIOC
-#define SCL_PIN 2
-
-#define PDM_CLK_PORT GPIOC
-#define PDM_CLK_PIN 5
-
-#define PDM_DATA_PORT GPIOC
-#define PDM_DATA_PIN 7
-
-#define AUDIO_PORT GPIOA
-#define AUDIO_PIN 1
-
-#define AMP_SD_PORT GPIOA
-#define AMP_SD_PIN 2
+#define AMP_SD_PORT GPIOD
+#define AMP_SD_PIN 3
 
 const uint8_t sine8[8] = {128, 218, 255, 218, 128, 37, 0, 37};
 const uint8_t sine16[16] = {128, 176, 218, 245, 255, 245, 218, 176, 128, 79, 37, 10, 0, 10, 37, 79};
@@ -63,132 +39,80 @@ void TIM2_IRQHandler(void)
 	TIM2->INTFR &= ~TIM_UIF; // Clear ISR flag
 	NVIC_ClearPendingIRQ(TIM2_IRQn);
 	sine_idx = (sine_idx < (WAVE_TABLE_LENGTH-1)) ? sine_idx+1 : 0;
-	TIM1->CH2CVR = WAVE_TABLE_NAME[sine_idx] / 8;
-
-	// AUDIO_PORT->OUTDR ^= (1<<AUDIO_PIN);
-}
-
-volatile uint8_t foo = 255;
-
-// Apparently this is neccesary 
-void TIM1_UP_IRQHandler(void) __attribute__((interrupt));
-void TIM1_UP_IRQHandler(void)
-{
-	TIM1->INTFR &= ~TIM_UIF; // Clear ISR flag
-	NVIC_ClearPendingIRQ(TIM1_UP_IRQn);
-	TIM1->CH2CVR = foo--; // allow roll-over
+	TIM1->CH1CVR = WAVE_TABLE_NAME[sine_idx];// / 8; // ###################### Put back in volume divider
 }
 
 int main()
 {
 	SystemInit(); // Default 48Mhz internal osc, 1kHz SysTick (no IRQ)
 
-	// Enable GPIOs
+	// Enable GPIO clocks
 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD;
 
-	// Disable audio amp (set high)
-	AMP_SD_PORT->CFGLR &= ~(0xf<<(4*AMP_SD_PIN));
-	AMP_SD_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*AMP_SD_PIN);
-	AMP_SD_PORT->BSHR = (1<<AMP_SD_PIN);
+	// ##### Setup TIM1 to generate PWM #####
+	// PA1 is T1CH2, 10MHz Output alt func, push-pull
+	AUDIO_PORT->CFGLR &= ~(0xf<<(4*AUDIO_PIN));
+	AUDIO_PORT->CFGLR |= (GPIO_Speed_2MHz | GPIO_CNF_OUT_PP_AF)<<(4*AUDIO_PIN);
 
-	// // LED pins as GPIO push-pull outputs
-	// // LED_R_PORT->CFGLR &= ~(0xf<<(4*LED_R_PIN));
-	// // LED_R_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*LED_R_PIN);
-	// // LED_G_PORT->CFGLR &= ~(0xf<<(4*LED_G_PIN));
-	// // LED_G_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*LED_G_PIN);
-	// // LED_B_PORT->CFGLR &= ~(0xf<<(4*LED_B_PIN));
-	// // LED_B_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*LED_B_PIN);
-
-	// // Setup TIM1 to generate PWM
-	// RCC->APB2PCENR |= RCC_APB2Periph_TIM1;
+	// Enable clock and reset TIM1
+	RCC->APB2PCENR |= RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
 	
-	// // // PA1 is T1CH2, 10MHz Output alt func, push-pull
-	// AUDIO_PORT->CFGLR &= ~(0xf<<(4*AUDIO_PIN));
-	// AUDIO_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF)<<(4*AUDIO_PIN);
-		
-	// // AUDIO_PORT->CFGLR &= ~(0xf<<(4*AUDIO_PIN));
-	// // AUDIO_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*AUDIO_PIN);
-
-	// // Reset TIM1 to init all regs
-	// RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
-	// RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+	// TIM1 PWM Frequency (48MHz / 2x prescaler/ 256 data = 93.75kHz)
+	TIM1->PSC = 1;
+	TIM1->ATRLR = 255;
 	
-	// // ---INVALID--- Auto Reload - sets period (48MHz fclk / 2 prescaler / 256 data / 16 wave table = 5859.375 Hz)
+	// Enable CH1 output, positive pol
+	TIM1->CCER |= TIM_CC1E | TIM_CC1P;
 
-	// // TIM1 PWM Frequency (48MHz / 2x prescaler/ 256 data = 93.75kHz)
-	// TIM1->PSC = 1;
-	// TIM1->ATRLR = 255;
+	// CH1 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1 | TIM_OC1PE;
 	
-	// // Enable CH2 output, positive pol
-	// TIM1->CCER |= TIM_CC2E | TIM_CC2P;
-
-	// // CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
-	// TIM1->CHCTLR1 |= TIM_OC2M_2 | TIM_OC2M_1 | TIM_OC2PE;
+	// Set the Capture Compare Register to zero bias
+	TIM1->CH1CVR = 10;
 	
-	// // Set the Capture Compare Register to zero bias
-	// TIM1->CH2CVR = 127;
+	// Reload immediately
+	TIM1->SWEVGR |= TIM_UG;
+
+	// Enable TIM1 outputs
+	TIM1->BDTR |= TIM_MOE;
 	
-	// // Reload immediately
-	// TIM1->SWEVGR |= TIM_UG;
+	// Enable TIM1
+	TIM1->CTLR1 |= TIM_ARPE | TIM_CEN;
 
-	// // Enable TIM1 outputs
-	// TIM1->BDTR |= TIM_MOE;
+	// ##### Setup TIM2 to advance the wave table #####
+	// Enable clock and reset TIM2
+	RCC->APB1PCENR |= RCC_APB1Periph_TIM2;
+	RCC->APB1PRSTR |= RCC_APB1Periph_TIM2;
+	RCC->APB1PRSTR &= ~RCC_APB1Periph_TIM2;
 	
-	// // TIM1->DMAINTENR |= TIM_UIE; // Debugging only
-
-	// // Enable TIM1
-	// TIM1->CTLR1 |= TIM_ARPE | TIM_CEN;
-
-
-	// // Setup TIM2 to advance the wave table
-	// RCC->APB1PCENR |= RCC_APB1Periph_TIM2;
-
-	// // Reset TIM2 to init all regs
-	// RCC->APB1PRSTR |= RCC_APB1Periph_TIM2;
-	// RCC->APB1PRSTR &= ~RCC_APB1Periph_TIM2;
+	// Prescaler and frequency
+	TIM2->PSC = 0;
+ 	set_freq(1000);
 	
-	// // Prescaler
-	// TIM2->PSC = 0;
+	// Enable update interrupt
+	TIM2->DMAINTENR |= TIM_UIE;
+	NVIC_EnableIRQ(TIM2_IRQn);
 	
- 	// set_freq(1000);
- 
-	// // TIM2->ATRLR = 10000;
-	// // TIM2->SWEVGR |= TIM_UG; // Reload timer immediately
-	
-	// // Enable update interrupt
-	// TIM2->DMAINTENR |= TIM_UIE;
-	
-	// // Enable TIM2
-	// TIM2->CTLR1 |= TIM_CEN;
+	// Enable TIM2
+	TIM2->CTLR1 |= TIM_CEN;
 
-	// NVIC_EnableIRQ(TIM2_IRQn);
+	__enable_irq();
 
-	// // NVIC_EnableIRQ(TIM1_UP_IRQn);
-
-	// __enable_irq();
-
-	// Enable audio amp
-	//AMP_SD_PORT->BCR = (1<<AMP_SD_PIN);
-
-	// uint16_t freq_hz = 100;
+	// PA2 is heartbeat
+    AMP_SD_PORT->CFGLR &= ~(0xf<<(4*AMP_SD_PIN));
+    AMP_SD_PORT->CFGLR |= (GPIO_Speed_2MHz | GPIO_CNF_OUT_PP)<<(4*AMP_SD_PIN);
+	AMP_SD_PORT->BSHR = (1<<(AMP_SD_PIN+16));
 	while(1)
 	{
-		__WFI();
-		// set_freq(freq_hz);
-		// Delay_Ms(500);
-		// freq_hz = (freq_hz < 5800) ? freq_hz + 100 : 100;
-
-
-		// LED_R_PORT->BCR = (1<<LED_R_PIN); // Red On
-		// Delay_Ms(3000); // Polling, doesn't use SysTick IRQ
-		// LED_R_PORT->BSHR = (1<<LED_R_PIN); // Red Off
-		// LED_G_PORT->BCR = (1<<LED_G_PIN); // Green On
-		// Delay_Ms(3000);
-		// LED_G_PORT->BSHR = (1<<LED_G_PIN); // Green Off
-		// LED_B_PORT->BCR = (1<<LED_B_PIN); // Blue On
-		// Delay_Ms(3000);
-		// LED_B_PORT->BSHR = (1<<LED_B_PIN); // Blue Off
-		// Delay_Ms(3000);
+		//__WFI();
+		Delay_Ms(500);
+		// Set pin (led off)
+		AMP_SD_PORT->BSHR = (1<<AMP_SD_PIN);
+		Delay_Ms(500);
+		// Clear pin (led on)
+		AMP_SD_PORT->BSHR = (1<<(AMP_SD_PIN+16));
 	}
 }
 
